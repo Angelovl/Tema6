f16b5bb98dc981978ff928a2d7710c33a67eb690
Angelo Visentin <123060347+Angelovl@users.noreply.github.com>
2023-02-06 23:35:59 +0100

    Add files via upload

---
+++ Potito/potito1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Potito;
+
+/**
+ *
+ * @author Angelo
+ */
+public class potito1 {
+    
+    //HA FUNCIONADO EL TRUCO DE SRC QUE ES LA CARPETA PARA VER LAS COSAS QUE HAGO CON NETBEANS
+    
+    
+}
